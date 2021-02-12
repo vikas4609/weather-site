38262807603da5fb152fca121ca5c4d8685ca1ff
Vikas Goyal <vikasg.cs.17@nitj.ac.in>
2021-02-12 09:37:48 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req, res) => {
+	res.render('index',{
+		title: 'weather',
+		name: 'vikas'
+	})
+})
+
+app.get('/about',(req, res) => {
+	res.render('about',{
+		'title': 'about',
+		'name': 'vikas'
+	})
+})
+
+app.get('/help',(req,res)=>{
+	res.render('help',{
+		title:'help',
+		msg:'help everyone',
+		name: 'vikas'
+	})
+})
+
+
+app.get('/weather',(req,res) => {
+
+	if(!req.query.address)
+	{
+		return res.send({
+			error : 'you must provide an address'
+		})
+	}
+
+	geocode(req.query.address, (error, {latitude, longitude, location}={}) => {
+		if(error){
+			return res.send({error})
+		}
+		forecast(latitude,longitude,(error, forecastData) => {
+			if(error) {
+				return res.send({error})
+			}
+			
+			res.send({
+				forecast: forecastData,
+				location: location,
+				address: req.query.address
+			})
+		})
+	})
+
+	
+})
+
+app.get('/help/*',(req,res)=>{
+	res.render('404',{
+		errorMsg:'help article not found',
+		name: 'vikas',
+		title: '404'
+	})
+})
+
+app.get('*',(req,res)=>{
+	res.render('404',{
+		errorMsg:'page not found',
+		name: 'vikas',
+		title: '404'
+	})
+})
+
+app.listen(3000, ()=>{
+	console.log('server listening on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+	const url = 'http://api.weatherstack.com/current?access_key=881e55cf0c39467644c5ad3f4a6e3884&query=' + latitude + ',' + longitude
+
+	request({url: url, json:true}, (error, response) => {
+		if(error) {
+			callback('unable to connect api', undefined)
+		}
+		else if(response.body.error){
+			callback('unable to fetch location. try again', undefined)
+		}
+		else {
+			callback(undefined, 'there is ' + response.body.current.temperature + ' degree temperature. It feels like ' + response.body.current.feelslike + ' degrees out there.')
+		}
+	})
+}
+
+module.exports = forecast

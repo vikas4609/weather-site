63bddd529f31bd4662bc463cfd7d5b1353362c22
Vikas Goyal <vikasg.cs.17@nitj.ac.in>
2021-02-12 11:55:28 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = app.env.PORT || 3000
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialPath = path.join(__dirname, '../templates/partials')
